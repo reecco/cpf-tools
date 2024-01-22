9d929fa91a4e658df208edfa828f11fbfd4c71a2
Fred Recco <fredcardoso01@hotmail.com>
2024-01-22 00:32:50 -0300

    update response message endpoint home

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const cnpjRoutes = require('./cnpj.routes');
 const cpfRoutes = require('./cpf.routes');
 
 module.exports = (app) => {
-  app.route('/').get((req, res) => res.status(200).json({ message: 'CPF API', code: 200 }));
+  app.route('/').get((req, res) => res.status(200).json({ message: 'CPF API' }));
 
   app.use(
     '/api/v2',
